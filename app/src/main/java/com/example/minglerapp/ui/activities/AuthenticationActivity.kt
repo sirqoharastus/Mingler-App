f903d716e833ab13508f6ec3ee4aff0033e1aa53
Abdulqohar <sabdulqohar9@gmail.com>
2021-06-29 14:02:43 +0100

    set up navigation component

--- app/src/main/java/com/example/minglerapp/ui/activities/AuthenticationActivity.kt
+++ app/src/main/java/com/example/minglerapp/ui/activities/AuthenticationActivity.kt
@@ -1,12 +1,12 @@
-package com.example.minglerapp.ui.fragments.activities
+package com.example.minglerapp.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.minglerapp.R
 
-class MainActivity : AppCompatActivity() {
+class AuthenticationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_authentication)
     }
 }
