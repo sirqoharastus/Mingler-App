d88826526237f2bf849ef21b8517f7023bab0ec2
Abdulqohar <sabdulqohar9@gmail.com>
2021-06-29 15:15:51 +0100

    enabled view binding and set up splash screen

--- app/src/main/java/com/example/minglerapp/ui/activities/AuthenticationActivity.kt
+++ app/src/main/java/com/example/minglerapp/ui/activities/AuthenticationActivity.kt
@@ -7,6 +7,7 @@ import com.example.minglerapp.R
 class AuthenticationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_MinglerApp)
         setContentView(R.layout.activity_authentication)
     }
 }
--- app/src/main/java/com/example/minglerapp/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/minglerapp/ui/fragments/LoginFragment.kt
@@ -5,17 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.minglerapp.R
+import com.example.minglerapp.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
+    var _binding : FragmentLoginBinding? = null
+    val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.startCodingButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registrationFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
