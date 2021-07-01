38094dc2503bfaa39e1ea01bbe7b40be99c10d28
Abdulqohar <sabdulqohar9@gmail.com>
2021-07-01 01:18:23 +0100

    started implementing reset password

--- app/src/main/java/com/example/minglerapp/ui/fragments/ForgotPasswordFragment.kt
+++ app/src/main/java/com/example/minglerapp/ui/fragments/ForgotPasswordFragment.kt
@@ -1,22 +1,48 @@
 package com.example.minglerapp.ui.fragments
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.minglerapp.R
+import com.example.minglerapp.databinding.FragmentForgotPasswordBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class ForgotPasswordFragment : Fragment() {
-
+    var _binding : FragmentForgotPasswordBinding? = null
+    val binding get() = _binding!!
+    private var mAuth: FirebaseAuth? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+        _binding = FragmentForgotPasswordBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mAuth = FirebaseAuth.getInstance()
 
+        binding.forgotPasswordResetPasswordButton.setOnClickListener {
+            resetPassword()
+        }
+    }
+
+    private fun resetPassword() {
+        if (binding.forgotPasswordEmailEdittext.text.toString().isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(binding.forgotPasswordEmailEdittext.text.toString()).matches()){
+            val email = binding.forgotPasswordEmailEdittext.text.toString()
+            mAuth?.sendPasswordResetEmail(binding.forgotPasswordEmailEdittext.text.toString().trim())
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
